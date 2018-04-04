8cc39eb957ec2566eaba394231a4e793268578b7
zyy <zhangyeyong2007@163.com>
2018-04-04 08:54:42 +0800

    添加测试java相关的工程    第一个是测试redis

---
+++ java_test/src/test/redis/RedisTest.java
@@ -0,0 +1,15 @@
+package test.redis;
+
+import redis.clients.jedis.Jedis;
+
+public class RedisTest {
+
+	public static void main(String[] args) {
+		 //连接本地的 Redis 服务
+        Jedis jedis = new Jedis("203.175.149.179",6379);
+        System.out.println("连接成功");
+        //查看服务是否运行
+        System.out.println("服务正在运行: "+jedis.ping());
+	}
+
+}
